e5b4ec29cc561d7b7b8a5f15db1e8b4e67209cf5
Uzurch1k <galcenkovlad7@gmail.com>
2024-12-20 19:45:46 +0200

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import './parts/modal.js';
 import './parts/mobmenu.js';
+import './parts/accordion.js';
 // import './parts/banner.js';
 // import './parts/splidecust.js';
 // import './parts/splide.js';
