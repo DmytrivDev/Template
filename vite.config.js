bd8959455741b028de143411b81dd6df69bcfdb7
Uzurch1k <galcenkovlad7@gmail.com>
2025-05-15 19:24:14 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,16 @@ import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig(({ command }) => {
   return {
+    root: 'src',
+
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    root: 'src',
+
     build: {
+      outDir: '../dist',
       sourcemap: true,
+      emptyOutDir: true,
       rollupOptions: {
         input: globSync('./src/*.html'),
         output: {
@@ -22,16 +26,40 @@ export default defineConfig(({ command }) => {
           },
           entryFileNames: chunkInfo => {
             if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
+              return 'js/commonHelpers.js';
             }
-            return '[name].js';
+            return 'js/[name].[hash].js';
+          },
+          chunkFileNames: chunkInfo => {
+            if (chunkInfo.name === 'vendor') {
+              return 'js/vendor.js';
+            }
+            return 'js/[name].[hash].js';
+          },
+          assetFileNames: assetInfo => {
+            const name = assetInfo.names ? assetInfo.names[0] : '';
+            const ext = name.split('.').pop();
+
+            if (/\.(png|jpe?g|gif|webp|bmp|tiff)$/.test(name)) {
+              return 'assets/images/content/[name].[hash].[ext]';
+            }
+            if (/\.svg$/.test(name)) {
+              return 'assets/images/icons/[name].[hash].[ext]';
+            }
+            if (/\.(woff2?|ttf|otf|eot)$/.test(name)) {
+              return 'assets/fonts/[name].[hash].[ext]';
+            }
+            if (ext === 'css') {
+              return 'css/[name].[hash].[ext]';
+            }
+            return 'assets/other/[name].[hash].[ext]';
           },
         },
       },
-      outDir: '../dist',
-      emptyOutDir: true,
     },
+
     plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+
     css: {
       preprocessorOptions: {
         sass: {
